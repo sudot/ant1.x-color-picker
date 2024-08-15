8d50a712abbbf13ecdf3d1cf5cc1e61a52b03579
唐家林 <taogarfield@hotmail.com>
2024-08-15 16:12:47 +0800

    优化文档

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # antdv1.x-color-picker
 
+[国内 Gitee 仓库](https://gitee.com/sudot/antdv1.x-color-picker) |
+[全球 Gihub 仓库](https://github.com/sudot/antdv1.x-color-picker)
+
 ## 前言
 
 ant-design-vue 的 UI 框架中，并没有 colorPicker 颜色选择器。找了一圈没有找到合适的支持 vue2 的颜色选择组件，最终找到了 [https://gitee.com/ghkeeping/ant-color-picker](https://gitee.com/ghkeeping/ant-color-picker)，并在其源码基础上修改后封装了该组件。
@@ -23,31 +26,6 @@ ant-design-vue 的 UI 框架中，并没有 colorPicker 颜色选择器。找了
 - 添加了面板中切换 `color-format` 功能。
 - 添加了选择器外部输入框，并优化了外部和内部输入框的独立逻辑。
 
-## 文档
-
-### 使用方法
-
-1. 将 `src/components/color-picker` 整个目录拷贝至你项目中任意位置。
-2. 在你的要使用此组件的页面中，引入 `src/components/color-picker/index.vue` 即可。
-
-引用示例可以参考 `src\App.vue`
-
-### Attributes
-
-| 参数          | 说明                    | 类型   | 可选值                                                                                                                        | 默认值                                                |
-| ------------- | ----------------------- | ------ | ----------------------------------------------------------------------------------------------------------------------------- | ----------------------------------------------------- |
-| value/v-model | 绑定值                  | string | —                                                                                                                             | —                                                     |
-| size          | 尺寸                    | string | large/default/small                                                                                                           | default                                               |
-| color-format  | 写入 v-model 的颜色格式 | string | hsl/hsv/hex/rgb                                                                                                               | hex（show-alpha 为 false）/ rgb（show-alpha 为 true） |
-| predefine     | 预定义颜色              | array  | —                                                                                                                             | —                                                     |
-| placement     | 颜色选择器弹出位置      | string | `top` `left` `right` `bottom` `topLeft` `topRight` `bottomLeft` `bottomRight` `leftTop` `leftBottom` `rightTop` `rightBottom` | bottomRight                                           |
-
-### Events
-
-| 事件名称 | 说明               | 回调参数 |
-| -------- | ------------------ | -------- |
-| change   | 当绑定值变化时触发 | 当前值   |
-
 ## 本地开发和运行
 
 本项目是一个完整的工程项目，可以运行并预览实际效果。运行此项目按以下标准流程即可：
@@ -56,3 +34,43 @@ ant-design-vue 的 UI 框架中，并没有 colorPicker 颜色选择器。找了
 npm install
 npm run serve
 ```
+
+## 使用方法
+
+1. 将 `src/components/color-picker` 整个目录拷贝至你项目中任意位置。
+2. 在你的要使用此组件的页面中，引入 `src/components/color-picker/index.vue` 即可。
+
+```html
+<template>
+  <color-picker v-model="color" />
+</template>
+
+<script>
+  import ColorPicker from '@/components/color-picker';
+
+  export default {
+    components: { ColorPicker },
+    data() {
+      return {
+        color: '#ff0000',
+      };
+    },
+  };
+</script>
+```
+
+### 属性
+
+| 参数          | 说明                    | 类型   | 可选值                                                                                                                        | 默认值      |
+| ------------- | ----------------------- | ------ | ----------------------------------------------------------------------------------------------------------------------------- | ----------- |
+| value/v-model | 绑定值                  | string | —                                                                                                                             | —           |
+| size          | 尺寸                    | string | large/default/small                                                                                                           | default     |
+| color-format  | 写入 v-model 的颜色格式 | string | hsl/hsv/hex/rgb                                                                                                               | rgb         |
+| predefine     | 预定义颜色              | array  | —                                                                                                                             | —           |
+| placement     | 颜色选择器弹出位置      | string | `top` `left` `right` `bottom` `topLeft` `topRight` `bottomLeft` `bottomRight` `leftTop` `leftBottom` `rightTop` `rightBottom` | bottomRight |
+
+### 事件
+
+| 事件名称 | 说明               | 回调参数 |
+| -------- | ------------------ | -------- |
+| change   | 当绑定值变化时触发 | 当前值   |
--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,213 @@
 <template>
   <div id="app">
-    <a-form-model
-      ref="ruleForm"
-      class="form"
-      :model="ruleForm"
-      :labelCol="{ span: 4 }"
-      :wrapperCol="{ span: 14 }"
-    >
-      <a-form-model-item label="Color1">
-        <a-row :gutter="[8, 8]">
-          <a-col :span="12">
-            <color-picker
-              v-model="ruleForm.color"
-              show-alpha
-              placement="bottomLeft"
-              :predefine="predefineColors"
-            />
-          </a-col>
-          <a-col :span="12" :style="{ color: ruleForm.color }">
-            回显：{{ ruleForm.color }}
-          </a-col>
-        </a-row>
-      </a-form-model-item>
-      <a-form-model-item label="Color2">
-        <a-row :gutter="[8, 8]">
-          <a-col :span="12">
-            <color-picker
-              v-model="ruleForm.color2"
-              show-alpha
-              colorFormat="hex"
-              placement="bottomLeft"
-              :predefine="predefineColors"
-            />
-          </a-col>
-          <a-col :span="12" :style="{ color: ruleForm.color2 }">
-            回显：{{ ruleForm.color2 }}
-          </a-col>
-        </a-row>
-      </a-form-model-item>
-    </a-form-model>
-    <a-row>
-      <a-col :span="5"><h2>文档和源码：</h2></a-col>
-      <a-col :span="12">
-        <div class="social-links">
+    <div class="page-header">
+      <h1>antdv1.x-color-picker</h1>
+      <h2>基于 Vue2 和 Ant Design of Vue 1.x 封装的颜色选择器组件</h2>
+      <a
+        href="https://gitee.com/sudot/antdv1.x-color-picker"
+        class="btn"
+        target="_blank"
+      >
+        View on Gitee
+      </a>
+      <a
+        href="https://github.com/sudot/antdv1.x-color-picker"
+        class="btn"
+        target="_blank"
+      >
+        View on GitHub
+      </a>
+    </div>
+    <div class="page-content">
+      <h1>antdv1.x-color-picker</h1>
+      <p>
+        ant-design-vue 的 UI 框架中，并没有 ColorPicker
+        颜色选择器。找了一圈没有找到合适的支持 Vue2 的颜色选择组件，最终找到了
+        <code>
           <a
-            href="https://gitee.com/sudot/antdv1.x-color-picker"
-            title="gitee"
+            href="https://gitee.com/ghkeeping/ant-color-picker"
             target="_blank"
-            rel="noreferrer noopener"
-            class="social-link"
           >
-            <img
-              alt="gitee logo"
-              src="https://gitee.com/assets/favicon.ico"
-              decoding="async"
-              style="width: 100%; height: 100%"
-            />
+            https://gitee.com/ghkeeping/ant-color-picker
           </a>
-          <a
-            href="https://github.com/sudot/antdv1.x-color-picker"
-            title="GitHub"
-            target="_blank"
-            rel="noreferrer noopener"
-            class="social-link"
-          >
-            <a-icon type="github" />
-          </a>
-        </div>
-      </a-col>
-    </a-row>
+        </code>
+        ，并在其源码基础上修改后适配 Vue2 和 Antdv1.x 封装了该组件。
+      </p>
+      <h2>示例</h2>
+      <a-form-model
+        ref="ruleForm"
+        class="form"
+        :model="ruleForm"
+        :labelCol="{ span: 4 }"
+        :wrapperCol="{ span: 14 }"
+      >
+        <a-form-model-item label="Color1">
+          <a-row :gutter="[8, 8]">
+            <a-col :span="12">
+              <color-picker
+                v-model="ruleForm.color"
+                show-alpha
+                placement="bottomLeft"
+                :predefine="predefineColors"
+              />
+            </a-col>
+            <a-col :span="12" :style="{ color: ruleForm.color }">
+              回显：{{ ruleForm.color }}
+            </a-col>
+          </a-row>
+        </a-form-model-item>
+        <a-form-model-item label="Color2">
+          <a-row :gutter="[8, 8]">
+            <a-col :span="12">
+              <color-picker
+                v-model="ruleForm.color2"
+                show-alpha
+                colorFormat="hex"
+                placement="bottomLeft"
+                :predefine="predefineColors"
+              />
+            </a-col>
+            <a-col :span="12" :style="{ color: ruleForm.color2 }">
+              回显：{{ ruleForm.color2 }}
+            </a-col>
+          </a-row>
+        </a-form-model-item>
+      </a-form-model>
+      <h2>使用方法</h2>
+      <ol>
+        <li>
+          将
+          <code>src/components/color-picker</code>
+          整个目录拷贝至你项目中任意位置。
+        </li>
+        <li>
+          在你的要使用此组件的页面中，引入
+          <code>src/components/color-picker/index.vue</code>
+          即可。
+        </li>
+      </ol>
+      <pre><code class="language-html">&lt;template&gt;
+  &lt;color-picker v-model=&quot;color&quot; /&gt;
+&lt;/template&gt;
+
+&lt;script&gt;
+  import ColorPicker from '@/components/color-picker';
+
+  export default {
+    components: { ColorPicker },
+    data () {
+      return {
+        color: &#x27;#ff0000&#x27;
+      }
+    }
+  }
+&lt;/script&gt;</code></pre>
+      <h2>属性</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>属性</th>
+            <th>说明</th>
+            <th>类型</th>
+            <th>可选值</th>
+            <th>默认值</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>value/v-model</td>
+            <td>绑定值</td>
+            <td>string</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>size</td>
+            <td>尺寸</td>
+            <td>string</td>
+            <td>
+              <code>large</code>
+              <code>default</code>
+              <code>small</code>
+            </td>
+            <td><code>default</code></td>
+          </tr>
+          <tr>
+            <td>color-format</td>
+            <td>写入 v-model 的颜色格式</td>
+            <td>string</td>
+            <td>
+              <code>hsl</code>
+              <code>hsv</code>
+              <code>hex</code>
+              <code>rgb</code>
+            </td>
+            <td><code>rgb</code></td>
+          </tr>
+          <tr>
+            <td>predefine</td>
+            <td>预定义颜色</td>
+            <td>Array&lt;string&gt;</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>placement</td>
+            <td>颜色选择器弹出位置</td>
+            <td>string</td>
+            <td>
+              <code>top</code>
+              <code>left</code>
+              <code>right</code>
+              <br />
+              <code>bottom</code>
+              <code>topLeft</code>
+              <br />
+              <code>topRight</code>
+              <code>bottomLeft</code>
+              <br />
+              <code>bottomRight</code>
+              <code>leftTop</code>
+              <br />
+              <code>leftBottom</code>
+              <code>rightTop</code>
+              <br />
+              <code>rightBottom</code>
+            </td>
+            <td><code>bottomRight</code></td>
+          </tr>
+        </tbody>
+      </table>
+      <h2>事件</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>事件名称</th>
+            <th>说明</th>
+            <th>回调参数</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>change</td>
+            <td>当绑定值变化时触发</td>
+            <td>当前值</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="page-footer">
+      antdv1.x-color-picker 组件由
+      <a href="https://sudot.net/antdv1.x-color-picker/" target="_blank">
+        sudot
+      </a>
+      编写
+    </div>
   </div>
 </template>
 
 <script>
+import hljs from 'highlight.js/lib/common';
 import ColorPicker from '@/components/color-picker';
 
 export default {
@@ -105,29 +240,22 @@ export default {
       ],
     };
   },
+  mounted() {
+    hljs.highlightAll();
+  },
 };
 </script>
 
 <style>
-#app {
-  width: 800px;
-  padding-top: 50px;
+@import 'highlight.js/styles/github.min.css';
+@import 'assets/main.css';
+
+table > thead > tr > th {
+  text-align: center;
 }
-.social-links {
-  display: flex;
-  align-items: center;
-}
-.social-link {
-  display: block;
-  color: #303133;
-  width: 30px;
-  height: 30px;
-  margin: 0 10px;
-}
-.social-link .anticon {
-  font-size: 30px;
-}
-.social-link:hover {
-  color: #303133;
+table > thead > tr > th,
+table > tbody > tr > td {
+  padding: 5px 10px;
+  border: 1px solid #d0d7de;
 }
 </style>
